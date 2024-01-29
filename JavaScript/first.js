1f79aa1607e45f4924b313a6579649a74f4b606b
Hirtik Malvi <hirtik.malvi@gmail.com>
2024-01-30 00:17:43 +0530

    JS till DOM

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,5 +1,19 @@
-let enteredName = prompt("Enter your full name without space: ");
+// console.log(document);
+// console.dir(document.getElementsByTagName("p"));
+// let firstEll = document.querySelector(".myBox");
+// let allEll = document.querySelectorAll(".myBox");
+// console.log(firstEll);
+// console.log(allEll);
+// console.log(firstEll.tagName);
+// let inText = document.querySelector("div");
+// console.log(document.querySelector("div").innerText);
+// console.log(document.querySelector("div").innerHTML);
+// console.log(document.querySelector("div").textContent);
 
-let userName = "@" + enteredName + enteredName.length;
+// console.dir(document.body.children[0]);
+// console.dir(document.body.firstElementChild);
+// console.dir(document.body.lastElementChild);
 
-console.log("Username is:", userName);
+let h2 = document.querySelector("h2");
+console.log(h2.innerText);
+h2.innerText = h2.innerText + " from Hirtik Malvi"
