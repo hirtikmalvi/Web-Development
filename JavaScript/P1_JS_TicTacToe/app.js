026c79d8ab2465c56d5f719616a3b7d37d8671c1
Hirtik Malvi <hirtik.malvi@gmail.com>
2024-02-11 01:15:06 +0530

    Rock Paper Scissor JS Project

--- JavaScript/P1_JS_TicTacToe/app.js
+++ JavaScript/P1_JS_TicTacToe/app.js
@@ -8,15 +8,16 @@ let isDraw = true;
 let totalNoOfMoves = 0;
 
 const winninPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnOfO) {
             box.innerHTML = "O";
+            box.style.color = "green";
             turnOfO = false;
         }
         else {
             box.innerHTML = "X";
+            box.style.color = "red";
             turnOfO = true;
         }
         box.disabled = true;
