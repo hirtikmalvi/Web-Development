f26258d1f2c5b3ba9d9cc07851e21b0f9af17ab0
Hirtik Malvi <hirtik.malvi@gmail.com>
2024-03-10 21:29:31 +0530

    2.45 hr

--- P3_Spotify_Clone/script.js
+++ P3_Spotify_Clone/script.js
@@ -1,6 +1,18 @@
 // let link = "http://127.0.0.1:5500/P3_Spotify_Clone/songs/.mp3";
 let playCurrentSongBtn = document.querySelector(".playCurrentSong");
 
+function secondsToMinutesSeconds(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+let currentSong = new Audio();
+
 async function getSong() {
     let response = await fetch("http://127.0.0.1:5500/P3_Spotify_Clone/songs/")
     let data = await response.text()
@@ -17,17 +29,28 @@ async function getSong() {
     return songs;
 }
 
+const playMusic = (track, pause = false) => {
+    currentSong.src = `http://127.0.0.1:5500/P3_Spotify_Clone/songs/${track}.mp3`;
+    if (!pause) {
+        currentSong.play();
+        playCurrentSongBtn.src = "pauseCurrentSong.svg";
+    }
+    document.querySelector(".songinfo").innerHTML = decodeURI(track).split(".mp3")[0];
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+}
+
 async function main() {
     var songs = await getSong();
-    console.log(songs);
+    // console.log(songs[0]);
+    playMusic(songs[0].split(".mp3")[0], true);
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li>
         <img class="invert" src="music.svg" alt="">
         <div class="info">
-            <div>${song.replaceAll("%20"," ").split(".mp3")[0].split(" -")[1]} by</div>
-            <div>${song.replaceAll("%20"," ").split(".mp3")[0].split(" -")[0]}</div>
+            <div>${song.replaceAll("%20", " ").split(".mp3")[0]}</div>
+            <div>${song.replaceAll("%20", " ").split(".mp3")[1]}</div>
         </div>
         <div class="playnow">
             <span>Play now</span>
@@ -36,12 +59,35 @@ async function main() {
     </li>`;
     }
 
-    playCurrentSongBtn.addEventListener("click", () => {
-        let audio = new Audio(songs[6]);
-        audio.play();
+    // console.log();
+    let arr = Array.from(document.querySelector(".songList").getElementsByTagName("ul"));
+    let insideArr = arr[0].getElementsByTagName("li");
+    Array.from(insideArr).forEach(e => {
+        e.addEventListener("click", () => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML);
+        });
     });
+
+
+    playCurrentSongBtn.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play();
+            playCurrentSongBtn.src = "pauseCurrentSong.svg";
+        }
+        else {
+            currentSong.pause();
+            playCurrentSongBtn.src = "playCurrentSong.svg";
+        }
+    });
+
+    currentSong.addEventListener("timeupdate", () => {
+        // console.log(currentSong.duration);
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".seekCircle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
+        document.querySelector(".seekLine2").style.width = (currentSong.currentTime / currentSong.duration) * 50 + "vw";
+
+    });
+
 }
-
-
 
 main();
